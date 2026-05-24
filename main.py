26ce6deed9c3ca1c2448c13f24377e537ac5d206
8p896rn2m7-glitch <8p896rn2m7@privaterelay.appleid.com>
2026-05-24 17:17:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import datetime
 
 BOT_TOKEN = "8910259457:AAHpwewaaZUEwxw8uGF8E0A2VKaev2UPxu0"
 CHAT_ID = "8699744629"
@@ -9,35 +10,25 @@ TELEGRAM_API = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 CHECK_INTERVAL = 8
 
 
-targets = [
-    {
-        "name": "富邦悍將",
-        "url": "https://guardians.fami.life/UTK0204_",
-        "zones": [
-            "B1_108","B1_109","B1_110","B1_112","B1_113","B1_114",
-            "B1_116","B1_117","B1_118",
-            "B1_111","B1_115",
-            "B1_102","B1_103","B1_106","B1_120","B1_123","B1_124",
-            "L2-211","L2_212","L2-214","L2_215",
-            "L4_401","L4-402","L4_403","L4-404","L4_405","L4_406",
-            "L4_407","L4_408","L4_410","L4_411","L4_412","L4_413",
-            "L4_414","L4_415","L4_416","L4_417",
-            "L5_503","L5_504","L5_505","L5_506","L5_507","L5_508",
-            "L5_509","L5_510","L5_511","L5_512","L5_513","L5_514","L5_515"
-        ]
-    },
-    {
-        "name": "耀燮場",
-        "url": "https://orders.ibon.com.tw/application/UTK02/UTK0201_000.aspx?PERFORMANCE_ID=B0B5MPY0&PRODUCT_ID=B0B5LWGN&strItem=WEB網站入口1",
-        "zones": ["A2"]
-    }
-]
-
-
-state = {
-    t["url"]: set()
-    for t in targets
+target = {
+    "name": "富邦悍將",
+    "url": "https://guardians.fami.life/UTK0204_",
+    "zones": [
+        "B1_108","B1_109","B1_110","B1_112","B1_113","B1_114",
+        "B1_116","B1_117","B1_118",
+        "B1_111","B1_115",
+        "B1_102","B1_103","B1_106","B1_120","B1_123","B1_124",
+        "L2-211","L2_212","L2-214","L2_215",
+        "L4_401","L4-402","L4_403","L4-404","L4_405","L4_406",
+        "L4_407","L4_408","L4_410","L4_411","L4_412","L4_413",
+        "L4_414","L4_415","L4_416","L4_417",
+        "L5_503","L5_504","L5_505","L5_506","L5_507","L5_508",
+        "L5_509","L5_510","L5_511","L5_512","L5_513","L5_514","L5_515"
+    ]
 }
+
+
+state_found = set()
 
 
 def send(msg):
@@ -60,17 +51,17 @@ def fetch(url):
         return ""
 
 
-def is_real_available(text, zone):
+def is_valid(text, zone):
     """
-    真正防誤報判斷
+    防誤報核心
     """
 
     if zone not in text:
         return False
 
-    bad = ["售完", "已售完", "無票", "不可選", "disabled", "sold out"]
+    bad_words = ["售完", "已售完", "無票", "不可選", "disabled", "輪椅"]
 
-    for b in bad:
+    for b in bad_words:
         if b in text:
             return False
 
@@ -78,35 +69,39 @@ def is_real_available(text, zone):
 
 
 while True:
-    for t in targets:
-        url = t["url"]
-        name = t["name"]
-        zones = t["zones"]
-
-        text = fetch(url)
+    try:
+        text = fetch(target["url"])
         if not text:
             continue
 
-        found = {z for z in zones if is_real_available(text, z)}
+        found = {
+            z for z in target["zones"]
+            if is_valid(text, z)
+        }
 
-        prev = state[url]
-        added = found - prev
+        new = found - state_found
 
-        if added:
-            msg = f"""🎫 釋票通知｜{name}
+        if new:
+            msg = f"""🎫 富邦悍將釋票通知
+
+🏟 主場例行賽｜臺北大巨蛋
 
 🟢 可購買區：
 """
 
-            for z in sorted(added):
+            for z in sorted(new):
                 msg += f"🟢 {z}\n"
 
-            msg += f"\n🔗 {url}"
+            msg += f"\n⏰ {datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')}"
+            msg += f"\n🔗 {target['url']}"
 
             send(msg)
 
-            print(name, "NEW:", added)
+            print("NEW:", new)
 
-        state[url] = found
+        state_found = found
+
+    except Exception as e:
+        print("error:", e)
 
     time.sleep(CHECK_INTERVAL)
