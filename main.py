fa69421f75233baae40cb52057ae713aa8af0c4b
8p896rn2m7-glitch <8p896rn2m7@privaterelay.appleid.com>
2026-05-24 13:59:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,30 @@ CHAT_ID = "8699744629"
 
 TELEGRAM_API = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-CHECK_INTERVAL = 10
+CHECK_INTERVAL = 8
 
 
 targets = [
     {
-        "name": "富邦場",
+        "name": "富邦悍將",
         "url": "https://guardians.fami.life/UTK0204_",
-        "zones": ["B1_108","B1_109","B1_110","B1_112","B1_113","B1_114","B1_116","B1_117","B1_118","內野"]
+        "zones": [
+            "B1_108","B1_109","B1_110","B1_112","B1_113","B1_114",
+            "B1_116","B1_117","B1_118",
+            "B1_111","B1_115",
+            "B1_102","B1_103","B1_106","B1_120","B1_123","B1_124",
+            "L2-211","L2_212","L2-214","L2_215",
+            "L4_401","L4-402","L4_403","L4-404","L4_405","L4_406",
+            "L4_407","L4_408","L4_410","L4_411","L4_412","L4_413",
+            "L4_414","L4_415","L4_416","L4_417",
+            "L5_503","L5_504","L5_505","L5_506","L5_507","L5_508",
+            "L5_509","L5_510","L5_511","L5_512","L5_513","L5_514","L5_515"
+        ]
     },
     {
-        "name": "ibon新場",
+        "name": "耀燮場",
         "url": "https://orders.ibon.com.tw/application/UTK02/UTK0201_000.aspx?PERFORMANCE_ID=B0B5MPY0&PRODUCT_ID=B0B5LWGN&strItem=WEB網站入口1",
-        "zones": ["A1","A2","A3"]
+        "zones": ["A2"]
     }
 ]
 
@@ -60,27 +71,25 @@ while True:
             prev = state[url]["found"]
 
             added = found - prev
-            removed = prev - found
 
-            # 🔥 有變化才通知
             if added:
-                msg = f"🎫 釋票更新｜{name}\n\n"
-                msg += "🆕 新出現區域：\n"
+                msg = f"🎫 釋票通知｜{name}\n\n"
+                msg += "🔥 新釋出區域：\n"
                 msg += "\n".join(sorted(added))
                 msg += f"\n\n🔗 {url}"
 
                 send(msg)
 
-            # 更新狀態
+                print(name, ":", added)
+
             state[url]["found"] = found
             state[url]["fail"] = 0
 
         except Exception as e:
             state[url]["fail"] += 1
-            print(f"{name} error:", e)
+            print(name, "error:", e)
 
-            # 連續失敗提醒
             if state[url]["fail"] == 5:
-                send(f"⚠️ {name} 監控異常（連線失敗）")
+                send(f"⚠️ {name} 監控異常")
 
     time.sleep(CHECK_INTERVAL)
